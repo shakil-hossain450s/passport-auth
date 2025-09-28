0a17f9f873178604d92144b3cc359e73bb638583
shakil <shakil.hossain450s@gmail.com>
2025-09-28 16:05:35 +0600

    register user function added

---
+++ model/users.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = new mongoose.model("Users", usersSchema);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const Users = require("../model/users.model");
 const router = express.Router();
 
 router.get("/register", (req, res) => {
-    res.status(200).render("register", {title: "Register"})
+    res.status(200).render("register", {title: "Register"});
+});
+
+router.post("/register", async(req, res) => {
+    try{
+
+        const {username, email, password} = req.body;
+        const existingUser = await Users.findOne({email: email});
+        if(existingUser){
+            return res.status(409).json({
+                message: "user already exist"
+            })
+        }
+        const newUser = new Users({username, email, password});
+        await newUser.save();
+        res.status(201).json({
+            message: "User Created Successfully",
+            newUser
+        });
+
+    } catch(err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
 })
 
 module.exports = router;
